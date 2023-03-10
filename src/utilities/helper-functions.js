51ded7645aa5d02653053a2aa91068d4f9d617c8
Mikhail Lyubimtsev <mikhail.lyubimtsev@gmail.com>
2023-03-10 17:20:43 +0300

    validate uri - first segment length is {1,256}

--- src/utilities/helper-functions.js
+++ src/utilities/helper-functions.js
@@ -94,4 +94,4 @@ export const toDigestString = (o = {}, level = 1) => {
 }
 
 
-export const validateUri = (URI) => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/ig.test(URI)
+export const validateUri = (URI) => /https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_+.~#?&//=]*)/ig.test(URI)
